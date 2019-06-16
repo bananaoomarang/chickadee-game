f4112c2bbfc6515de8eecd84905d7b04b816178d
Milo Mordaunt <milomord@gmail.com>
2019-06-15 20:26:04 -0400

    Add asteroids physics instead

--- main.scm
+++ main.scm
@@ -10,37 +10,59 @@
 
 (define window-width 1280)
 (define window-height 960)
-(define SPRITE-SPEED 200.0)
+(define PI 3.14159265359)
+(define SPRITE-SPEED 10.0)
+(define SPRITE-ANG-SPEED 5.0)
 
 (define sprite #f)
+(define sprite-hw 64)
+(define sprite-hh 64)
+(define sprite-angular 0.0)
+(define sprite-rotation 0.0)
+(define sprite-acc 0.0)
 (define sprite-vel #v(0.0 0.0))
 (define sprite-pos #v(100.0 100.0))
 
 (define (sprite-reset!)
+  (set! sprite-angular 0.0)
+  (set! sprite-rotation 0.0)
+  (set! sprite-acc 0.0)
   (set-vec2! sprite-vel 0.0 0.0)
-  (set-vec2! sprite-pos 100.0 100.0))
+  (set-vec2! sprite-pos 600.0 100.0))
+
+(define (deg->rad d)
+  (* (/ PI 180) d))
+
+
+(define (get-direction rad)
+  "Get direction vector from radians IDK"
+
+  ;; IDK why the * -1 is necessary sorry! it's just the way it is baby
+  (let ((-rad (* -1 rad)))
+    #v((sin -rad) (cos -rad))))
 
 (define (sprite-update! dt)
+  (set! sprite-rotation (+ sprite-rotation (* dt sprite-angular)))
+
+  (vec2-add! sprite-vel (vec2* (get-direction sprite-rotation) sprite-acc))
+
   (set-vec2-x! sprite-pos (+ (vec2-x sprite-pos) (* dt (vec2-x sprite-vel))))
   (set-vec2-y! sprite-pos (+ (vec2-y sprite-pos) (* dt (vec2-y sprite-vel)))))
 
-
-(define (sprite-add-x n)
-  (set-vec2-x! sprite-vel (+ n (vec2-x sprite-vel))))
-(define (sprite-add-y n)
-  (set-vec2-y! sprite-vel (+ n (vec2-y sprite-vel))))
+(define (sprite-rot! n)
+  (set! sprite-angular (+ n sprite-angular)))
 
 (define key-press-handlers (make-hash-table))
-(hash-set! key-press-handlers 'up (lambda () (sprite-add-y SPRITE-SPEED)))
-(hash-set! key-press-handlers 'down (lambda () (sprite-add-y (* -1 SPRITE-SPEED))))
-(hash-set! key-press-handlers 'left (lambda () (sprite-add-x (* -1 SPRITE-SPEED))))
-(hash-set! key-press-handlers 'right (lambda () (sprite-add-x SPRITE-SPEED)))
+(hash-set! key-press-handlers 'up (lambda () (set! sprite-acc SPRITE-SPEED)))
+
+(hash-set! key-press-handlers 'left (lambda () (sprite-rot! SPRITE-ANG-SPEED)))
+(hash-set! key-press-handlers 'right (lambda () (sprite-rot! (* -1 SPRITE-ANG-SPEED))))
 
 (define key-release-handlers (make-hash-table))
-(hash-set! key-release-handlers 'up (lambda () (sprite-add-y (* -1 SPRITE-SPEED))))
-(hash-set! key-release-handlers 'down (lambda () (sprite-add-y SPRITE-SPEED)))
-(hash-set! key-release-handlers 'left (lambda () (sprite-add-x SPRITE-SPEED)))
-(hash-set! key-release-handlers 'right (lambda () (sprite-add-x (* -1 SPRITE-SPEED))))
+(hash-set! key-release-handlers 'up (lambda () (set! sprite-acc 0.0)))
+
+(hash-set! key-release-handlers 'left (lambda () (sprite-rot! (* -1 SPRITE-ANG-SPEED))))
+(hash-set! key-release-handlers 'right (lambda () (sprite-rot! SPRITE-ANG-SPEED)))
 
 (define (key-press key scancode modifiers repeat?)
   (let ((handler (hash-ref key-press-handlers key)))
@@ -53,15 +75,24 @@
 (define (load)
   (set! sprite (load-image "assets/images/chickadee.png")))
 
-(define (draw alpha)
-  (draw-sprite sprite sprite-pos))
+(define (game-draw alpha)
+  (draw-sprite sprite sprite-pos #:rotation sprite-rotation #:origin #v(sprite-hw sprite-hh)))
 
 (define (game-update dt)
   (let ((dt-seconds (/ dt 1000.0)))
     (sprite-update! dt-seconds)))
 
+(define (draw alpha)
+  "Call the game draw function but don't let it crash"
+
+  (catch #t
+    (lambda () (game-draw alpha))
+    (lambda (key . parameters)
+      (pretty-print key)
+      (pretty-print parameters))))
+
 (define (update dt)
-  "Call the game update but keep calling the repl poll if it breaks"
+  "Call the game update function but keep calling the repl poll if it breaks"
   
   (catch #t
     (lambda () (game-update dt))
